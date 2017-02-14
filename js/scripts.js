52aabd237196cd4cdd902897a587994f57d85be6
Chris Carr and Megan Warnock <chris@>
2017-02-14 10:14:25 -0800

    It will add dark theme button with styles and js.

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,16 @@ $(function() {
     $("#manatee-showing").fadeToggle();
     $("#manatee-hide").fadeToggle();
   });
-});
 
-$(function() {
   $(".clickable2").click(function() {
     $("#manatee2-showing").slideToggle();
   });
-});
 
-
-$(function() {
   $(".btn-clickable").click(function() {
     $(".castle-showing").slideToggle();
   });
+
+  $("button#dark").click(function() {
+    $("body").addClass("dark-background")
+  })
 });
